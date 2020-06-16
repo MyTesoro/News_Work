f4411012f2d921bc79bf7d1e54d83100a4578ae0
MyTesoro <36811080+MyTesoro@users.noreply.github.com>
2020-06-16 21:28:45 +0800

    ok

---
+++ info/modules/index/views.py
@@ -0,0 +1,14 @@
+from flask import render_template, current_app
+
+from . import index_blu
+
+
+@index_blu.route('/')
+def index():
+    # return "index"
+    return render_template('news/index.html')
+
+
+@index_blu.route('/favicon.ico')
+def get_web_logo():
+    return current_app.send_static_file('news/favicon.ico')
